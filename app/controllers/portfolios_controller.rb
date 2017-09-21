fe416da61d27c1b8555c7217707f4070fca1de91
Barry <accounts01@systemweavers.com>
2017-09-21 08:28:35 -0700

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,30 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  # POST /portfolios
+  # POST /portfolios.json
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
+
 end
